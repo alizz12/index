d0756344707b3cd889527a23cbca3d9a96271789
alizz12 <150627282+alizz12@users.noreply.github.com>
2023-11-21 01:32:44 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-// Specify the target URL
-$redirect_url = 'https://google.com';
-
-// Perform the redirect
-header("Location: $redirect_url");
-exit; // Ensure that subsequent code is not executed
+echo 'Hello Ali';
 ?>
