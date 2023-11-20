1f78e6786f94a3e013d3d876d46303b6f3b33707
alizz12 <150627282+alizz12@users.noreply.github.com>
2023-11-21 01:30:05 +0330

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-<?php
-// Specify the target URL
-$redirect_url = 'https://google.com';
-
-// Perform the redirect
-header("Location: $redirect_url");
-exit; // Ensure that subsequent code is not executed
-?>
